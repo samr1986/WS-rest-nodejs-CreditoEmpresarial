246b232ce7172c03642c1d72839548707b9bd452
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-21 16:25:46 -0500

    respuesta coneccion cosmos db

--- routes/login.js
+++ routes/login.js
@@ -14,23 +14,22 @@ let loginSchema = {
     }
 };
 
-let mongoConnect = mongoose.connect(process.env.COSMOSDB_CONNSTR + "?ssl=true&replicaSet=globaldb", {
-        auth: {
-            user: process.env.COSMODDB_USER,
-            password: process.env.COSMOSDB_PASSWORD
-        }
-    })
-    .then(() => {
-        loginSchema.salida.cogigoRespuesta = 0;
-        loginSchema.salida.respuesta = 'conexion establecida con exito';
-    })
-    .catch((err) => {
-        loginSchema.salida.cogigoRespuesta = 200;
-        loginSchema.salida.respuesta = 'conexion no establecida ' + err;
-    });
-
 router.get('/', function(req, res, next) {
-    let conexion = mongoConnect.connection;
+    mongoose.connect(process.env.COSMOSDB_CONNSTR + "?ssl=true&replicaSet=globaldb", {
+            auth: {
+                user: process.env.COSMODDB_USER,
+                password: process.env.COSMOSDB_PASSWORD
+            }
+        })
+        .then(() => {
+            loginSchema.salida.cogigoRespuesta = 0;
+            loginSchema.salida.respuesta = 'conexion establecida con exito';
+        })
+        .catch((err) => {
+            loginSchema.salida.cogigoRespuesta = 200;
+            loginSchema.salida.respuesta = 'conexion no establecida ' + err;
+        });
+    let conexion = mongoose.connection;
     conexion.on('error', function() {
         loginSchema.salida.cogigoRespuesta = 400;
         loginSchema.salida.respuesta = 'error conectandose a cosmos db ';
