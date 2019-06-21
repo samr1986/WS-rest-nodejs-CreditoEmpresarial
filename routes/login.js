802b2cadf0c8724f8f130f2a271bd5ed473ffd00
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-21 11:15:09 -0500

    respuesta coneccion cosmos db

--- routes/login.js
+++ routes/login.js
@@ -15,23 +15,20 @@ let loginSchema = {
 };
 
 router.get('/', function(req, res, next) {
-    let resultadoConexion = false,
-        errorConeccion;
     mongoose.connect(process.env.COSMOSDB_CONNSTR + "?ssl=true&replicaSet=globaldb", {
             auth: {
                 user: process.env.COSMODDB_USER,
                 password: process.env.COSMOSDB_PASSWORD
             }
         })
-        .then(() => resultadoConexion = true)
-        .catch((err) => errorConeccion = err);
-    if (resultadoConexion) {
-        loginSchema.cogigoRespuesta = 0;
-        loginSchema.respuesta = 'conexion establecida con exito';
-    } else {
-        loginSchema.cogigoRespuesta = 200;
-        loginSchema.respuesta = 'conexion no establecida ' + errorConeccion;
-    }
+        .then(() => {
+            loginSchema.salida.cogigoRespuesta = 0;
+            loginSchema.salida.respuesta = 'conexion establecida con exito';
+        })
+        .catch((err) => {
+            loginSchema.salida.cogigoRespuesta = 200;
+            loginSchema.salida.respuesta = 'conexion no establecida ' + err;
+        });
     res.send(loginSchema)
 
 });
