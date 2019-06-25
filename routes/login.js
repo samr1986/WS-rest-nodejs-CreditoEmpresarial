c308cbfec8f3d12e465ce4ec4a6b9c2b0e517fe5
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-25 09:05:53 -0500

    respuesta coneccion cosmos db

--- routes/login.js
+++ routes/login.js
@@ -27,15 +27,14 @@ router.get('/', function(req, res, next) {
         .catch((err) => {
             loginSchema.salida.codigoRespuesta = 200;
             loginSchema.salida.respuesta = 'conexion no establecida ' + err;
-            res.send(loginSchema);
         });
     let conexion = mongoose.connection;
-    conexion.on('error', function() {
+    mongoose.connection.on('error', function() {
         loginSchema.salida.codigoRespuesta = 400;
         loginSchema.salida.respuesta = 'error conectandose a cosmos db ';
     });
-    conexion.on('open', function() {
-        conexion.db.collection("UsuariosColaboradores", function(err, collection) {
+    mongoose.connection.on('open', function() {
+        mongoose.connection.db.collection("UsuariosColaboradores", function(err, collection) {
             collection.find({ 'identificacion': loginSchema.entrada.usuario }).toArray(function(err, data) {
                 loginSchema.salida.codigoRespuesta = 500;
                 loginSchema.salida.respuesta = 'Logueo incorrecto';
