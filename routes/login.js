f8e8dc4ccb6b9d779c73a9012a078fdbae1f3fb0
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-25 08:22:20 -0500

    respuesta coneccion cosmos db

--- routes/login.js
+++ routes/login.js
@@ -3,18 +3,17 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var env = require('dotenv').config();
 
-let loginSchema = {
-    entrada: {
-        usuario: '',
-        password: ''
-    },
-    salida: {
-        codigoRespuesta: 200,
-        respuesta: '',
-    }
-};
-
 router.get('/', function(req, res, next) {
+    let loginSchema = {
+        entrada: {
+            usuario: '',
+            password: ''
+        },
+        salida: {
+            codigoRespuesta: 200,
+            respuesta: '',
+        }
+    };
     loginSchema.entrada.usuario = req.query.usuario;
     loginSchema.entrada.password = req.query.password;
     mongoose.set('bufferCommands', false);
@@ -24,36 +23,35 @@ router.get('/', function(req, res, next) {
                 password: process.env.COSMOSDB_PASSWORD
             }
         })
-        .then(() => {
-            let conexion = mongoose.connection;
-            conexion.on('error', function() {
-                loginSchema.salida.codigoRespuesta = 400;
-                loginSchema.salida.respuesta = 'error conectandose a cosmos db ';
-            });
-            conexion.once('open', function() {
-                conexion.db.collection("UsuariosColaboradores", function(err, collection) {
-                    collection.find({ 'identificacion': loginSchema.entrada.usuario }).toArray(function(err, data) {
-                        loginSchema.salida.codigoRespuesta = 500;
-                        loginSchema.salida.respuesta = 'Logueo incorrecto';
-                        if (data.length == 1) {
-                            if (data[0].password == loginSchema.entrada.password) {
-                                loginSchema.salida.codigoRespuesta = 0;
-                                loginSchema.salida.respuesta = 'Logueo existoso';
-                            }
-
-                        }
-                    })
-                });
-
-            });
-            res.send(loginSchema);
-        })
+        .then(() => {})
         .catch((err) => {
             loginSchema.salida.codigoRespuesta = 200;
             loginSchema.salida.respuesta = 'conexion no establecida ' + err;
             res.send(loginSchema);
         });
+    let conexion = mongoose.connection;
+    conexion.on('error', function() {
+        loginSchema.salida.codigoRespuesta = 400;
+        loginSchema.salida.respuesta = 'error conectandose a cosmos db ';
+    });
+    conexion.on('open', function() {
+        conexion.db.collection("UsuariosColaboradores", function(err, collection) {
+            collection.find({ 'identificacion': loginSchema.entrada.usuario }).toArray(function(err, data) {
+                loginSchema.salida.codigoRespuesta = 500;
+                loginSchema.salida.respuesta = 'Logueo incorrecto';
+                if (data.length == 1) {
+                    if (data[0].password == loginSchema.entrada.password) {
+                        loginSchema.salida.codigoRespuesta = 0;
+                        loginSchema.salida.respuesta = 'Logueo existoso';
+                    }
+
+                }
+            })
+        });
+
+    });
     mongoose.connection.close();
+    res.send(loginSchema);
 });
 
 module.exports = router;
