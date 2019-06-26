80d7f0e474c8d35c3364be490b86c4105e6865f6
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-26 16:24:15 -0500

    respuesta coneccion cosmos db

--- routes/login.js
+++ routes/login.js
@@ -22,20 +22,7 @@ router.get('/', function(req, res, next) {
     });
     mongoose.connection.once('open', function() {
         let coleccion = mongoose.connection.db.collection("UsuariosColaboradores");
-        coleccion.find({ 'identificacion': loginSchema.entrada.usuario }).toArray(function(err, data) {
-            loginSchema.salida.codigoRespuesta = 500;
-            loginSchema.salida.respuesta = 'Logueo incorrecto';
-            if (err) {
-                loginSchema.salida.codigoRespuesta = 600;
-                loginSchema.salida.respuesta = 'consulta con error';
-            }
-            if (data.length == 1) {
-                if (data[0].password == loginSchema.entrada.password) {
-                    loginSchema.salida.codigoRespuesta = 0;
-                    loginSchema.salida.respuesta = 'Logueo existoso';
-                }
-            }
-        });
+        realizarConsulta(colleccion);
     });
     mongoose.connect(process.env.COSMOSDB_CONNSTR + "?ssl=true&replicaSet=globaldb", {
             auth: {
@@ -52,5 +39,22 @@ router.get('/', function(req, res, next) {
             res.send(loginSchema);
         });
 });
+
+function realizarConsulta(coleccion) {
+    coleccion.find({ 'identificacion': loginSchema.entrada.usuario }).toArray(function(err, data) {
+        loginSchema.salida.codigoRespuesta = 500;
+        loginSchema.salida.respuesta = 'Logueo incorrecto';
+        if (err) {
+            loginSchema.salida.codigoRespuesta = 600;
+            loginSchema.salida.respuesta = 'consulta con error';
+        }
+        if (data.length == 1) {
+            if (data[0].password == loginSchema.entrada.password) {
+                loginSchema.salida.codigoRespuesta = 0;
+                loginSchema.salida.respuesta = 'Logueo existoso';
+            }
+        }
+    });
+}
 
 module.exports = router;
