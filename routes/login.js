fa7b8df52ee671f138e7652642a135af768a36ae
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-26 10:58:22 -0500

    respuesta coneccion cosmos db

--- routes/login.js
+++ routes/login.js
@@ -25,35 +25,6 @@ router.get('/', function(req, res, next) {
         })
         .then(() => {
             loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' conexion exitosa ';
-            mongoose.connection.on('error', function() {
-                loginSchema.salida.codigoRespuesta = 400;
-                loginSchema.salida.respuesta = 'error conectandose a cosmos db ';
-            });
-            mongoose.connection.once('open', function() {
-                loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' Entro al open ';
-                let coleccion = mongoose.connection.db.collection("UsuariosColaboradores");
-                coleccion.find({ 'identificacion': loginSchema.entrada.usuario }).toArray(function(err, data) {
-                    loginSchema.salida.codigoRespuesta = 500;
-                    loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' Logueo incorrecto';
-                    if (err) {
-                        loginSchema.salida.codigoRespuesta = 600;
-                        loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' consulta con error';
-                    }
-                    if (data) {
-                        loginSchema.salida.codigoRespuesta = 0;
-                        loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' consulta hecha ' + data;
-
-                    }
-                    /*if (data.length == 1) {
-                        if (data[0].password == loginSchema.entrada.password) {
-                            loginSchema.salida.codigoRespuesta = 0;
-                            loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' Logueo existoso';
-                            //mongoose.connection.close();
-                        }
-                    }*/
-                });
-                loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' ' + coleccion.namespace;
-            });
             res.send(loginSchema);
         })
         .catch((err) => {
@@ -61,6 +32,35 @@ router.get('/', function(req, res, next) {
             loginSchema.salida.respuesta = 'conexion no establecida ' + err;
             res.send(loginSchema);
         });
+    mongoose.connection.on('error', function() {
+        loginSchema.salida.codigoRespuesta = 400;
+        loginSchema.salida.respuesta = 'error conectandose a cosmos db ';
+    });
+    mongoose.connection.once('open', function() {
+        loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' Entro al open ';
+        let coleccion = mongoose.connection.db.collection("UsuariosColaboradores");
+        coleccion.find({ 'identificacion': loginSchema.entrada.usuario }).toArray(function(err, data) {
+            loginSchema.salida.codigoRespuesta = 500;
+            loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' Logueo incorrecto';
+            if (err) {
+                loginSchema.salida.codigoRespuesta = 600;
+                loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' consulta con error';
+            }
+            if (data) {
+                loginSchema.salida.codigoRespuesta = 0;
+                loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' consulta hecha ' + data;
+
+            }
+            /*if (data.length == 1) {
+                if (data[0].password == loginSchema.entrada.password) {
+                    loginSchema.salida.codigoRespuesta = 0;
+                    loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' Logueo existoso';
+                    //mongoose.connection.close();
+                }
+            }*/
+        });
+        loginSchema.salida.respuesta = loginSchema.salida.respuesta + ' ' + coleccion.namespace;
+    });
 });
 
 module.exports = router;
