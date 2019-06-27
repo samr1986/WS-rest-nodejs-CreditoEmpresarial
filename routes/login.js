91eb7db7bd54e36e9c37e3504e8437293c3bf6f5
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-27 17:26:15 -0500

    respuesta coneccion cosmos db

--- routes/login.js
+++ routes/login.js
@@ -30,7 +30,7 @@ router.get('/', function(req, res, next) {
             res.send(loginSchema);
         });*/
     loginSchema.salida.codigoRespuesta = 500;
-    loginSchema.salida.respuesta = 'que tiene usuariocola: ' + UsuarioColaborador.db;
+    loginSchema.salida.respuesta = 'que tiene usuariocola: ' + JSON.stringify(UsuarioColaborador.db);
     res.send(loginSchema);
 });
 
