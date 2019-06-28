de7c8eb0610e9c1730474e155b58cb70e57247f0
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-28 16:12:11 -0500

    se implementa primer servicio get de login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var loginRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var subsanarExcepcionesRouter = require('./routes/subsanarExcepciones');
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/subsanarExcepciones', subsanarExcepcionesRouter);
 
