fb4421357c6102b1f470d5a35832c9e5e0ac7251
Sergio Andres Melo Ramos <melos@bancoavvillas.com.co>
2019-06-28 09:36:31 -0500

    respuesta coneccion cosmos db

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+let mongoose = require('mongoose');
 router.get('/', function(req, res, next) {
     let loginSchema = {
         entrada: {
